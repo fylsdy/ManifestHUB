883105917173f4e8480544171c63c27399ab0878
fylsdy <167668065+fylsdy@users.noreply.github.com>
2025-09-02 11:23:15 +0800

    Add files via upload

---
+++ 2527390.lua
@@ -0,0 +1,59 @@
+--====================================================================================
+--APPID:2527390;Dead Rising Deluxe Remaster
+--====================================================================================
+addappid(2527391,1,"59f5483e65e1b578cfaea1da84860fc0988266ef8f2847637d6f2e9a3d9d3842") -- Depot 2527391
+--addappid(2527392,1,"Need Owner Share (JP Only)")
+addappid(2851890,1,"051f8d3634eacd7215b22a3690863f2775d426b67f7d391c37ffc149c8129337") -- Dead Rising Deluxe Remaster - Costume  BGM Licker - Depot 2851890
+addappid(2851900,1,"8a1835e29c15ae20fa73a08e65720b370e406d416a4cd2bd8b158227e4f6e9ac") -- Dead Rising Deluxe Remaster - Costume  BGM Nemesis - Depot 2851900
+addappid(2851910,1,"9ae538cb36e5b31e2a5efc8bb2308e5eac6c092e4821b01681bd8168115b2416") -- Dead Rising Deluxe Remaster - Costume  BGM Chainsaw Villager - Depot 2851910
+addappid(2851920,1,"7a6cda56c9e45752bd16b58ac73d075e04f37f7abfac654da8544c5581a2654b") -- Dead Rising Deluxe Remaster - Costume  BGM Leon S. Kennedy - Depot 2851920
+addappid(2851930,1,"2cfb7478360311fb64c9e6345175f6fda29df53216b19304a02449500a5ee2b4") -- Dead Rising Deluxe Remaster - Costume  BGM Ashley Graham - Depot 2851930
+addappid(2851940,1,"fe01a976754281d9a58e2ca86f1f35484fb5be574f34e4eeef776062976fc273") -- Dead Rising Deluxe Remaster - Costume  BGM Chris Redfield - Depot 2851940
+addappid(2851950,1,"69ee1ce0e7409e225f42465821dad21874700044b9f09d6e4a3f607139005839") -- Dead Rising Deluxe Remaster - Costume  BGM Tron Bonne - Depot 2851950
+addappid(2851960,1,"a18fba1a2a938c18ed2f67b2316c1ad92e1fd884a14a6d22d00203d7710405cf") -- Dead Rising Deluxe Remaster - Costume  BGM Lan Hikari - Depot 2851960
+addappid(2851970,1,"ae39d9f711c0054dfdee6e9da63e534cecf9b775e644d7da0022c8017879c133") -- Dead Rising Deluxe Remaster - Costume  BGM Mega Man - Depot 2851970
+addappid(2851980,1,"3734b60984f2f19e7ac3cbf143f6ccd9179e615a25a2f7e3e6f6259c5bad3d8a") -- Dead Rising Deluxe Remaster - Costume  BGM Sheriff - Depot 2851980
+addappid(2851990,1,"16f7e64bbe2c99e6ab01f0a922d238b3329a1079e247e29811a1ecc6834c3a71") -- Dead Rising Deluxe Remaster - Costume  BGM Clown - Depot 2851990
+addappid(2852000,1,"394d0e18c09d3e077bc69acbd973f7e4b0ac7f6a1347bc8409f5e216e769df30") -- Dead Rising Deluxe Remaster - Costume  BGM War Photographer - Depot 2852000
+addappid(2852010,1,"3f99da66c79385012ead7db222f9b014596ab6aa6c95723ad1e3fa8beb5bfbb1") -- Dead Rising Deluxe Remaster - Costume  BGM Creepy Rabbit - Depot 2852010
+addappid(2852020,1,"d7324bee6c82e7315392c72e905a8824a8d32df44faaf6050f90f924957bafda") -- Dead Rising Deluxe Remaster - Costume  BGM Ken - Depot 2852020
+addappid(2852030,1,"a0a05dc7ee0a017f074d2793448c4f161be8a20ab9c4af0846d5229e0e8aaed5") -- Dead Rising Deluxe Remaster - Costume  BGM Blodia - Depot 2852030
+addappid(2852040,1,"bd6be2e6c4111b1fcaff2954da650db7ee03e3a0ff254521ba4c4375a845b674") -- Dead Rising Deluxe Remaster - Costume  BGM Sodom - Depot 2852040
+addappid(2852050,1,"dfbc8ff76b92a0e7179f6efa67cc30b81ab6ee0a42f61da04e41e1825b2b27b6") -- Dead Rising Deluxe Remaster - Costume  BGM Rikuo - Depot 2852050
+addappid(2852060,1,"217699cde5a6f1b7e34481d957a325083aaf4e7bb52c629ae15b324d07469ff2") -- Dead Rising Deluxe Remaster - Costume  BGM Chuck Greene - Depot 2852060
+addappid(2852070,1,"db1c1d520c631d3a5e8a3d9e68c3c76540e98efcee75a666b0713b47f2f2ef46") -- Dead Rising Deluxe Remaster - Costume  BGM Frank West 2006 - Depot 2852070
+addappid(2852080,1,"c265f1c11629dd4f9a3d9f88c9d19c81fcd862cbcd6ef87afd0c553b94a79da1") -- Dead Rising Deluxe Remaster - Costume  BGM Willamette Parkview Mall Bee - Depot 2852080
+--====================================================================================
+--APPID:2892640;Dead Rising Deluxe Remaster Demo
+--====================================================================================
+--addappid(2892641,1,"Free On Demand")
+--addappid(2892642,1,"Free On Demand (JP Only)")
+--====================================================================================
+--APPID:3214130;DEAD RISING Deluxe Remaster - Ambient & Horror
+--====================================================================================
+--addappid(3214131,1,"Need Owner Share")
+--====================================================================================
+--APPID:3659690;DEAD RISING Deluxe Remaster - Cinematic
+--====================================================================================
+--addappid(3659691,1,"Need Owner Share")
+--====================================================================================
+--APPID:3659770;DEAD RISING Deluxe Remaster - Relax & Fun
+--====================================================================================
+--addappid(3659771,1,"Need Owner Share")
+--====================================================================================
+--APPID:2527390;DEAD RISING Deluxe Remaster - Rock & Alternative
+--====================================================================================
+--addappid(3659831,1,"Need Owner Share")
+--====================================================================================
+--APPID:3668680;どうせ僕らは (DEAD RISING Deluxe Remaster プロモーション曲)
+--====================================================================================
+--addappid(3668681,1,"Need Owner Share")
+--====================================================================================
+--OPTIONS
+--eg:
+--addappid(1196590)
+--setManifestid(1608283,"3451179779183755707",201047698)
+--addtoken(2786680,"16612582771884404058")
+--setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+--====================================================================================
+addappid(2527390) -- Dead Rising Deluxe Remaster
