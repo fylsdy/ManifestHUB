96c9e88f3c8a63a698da58391e21fb0e4aaf4187
fylsdy <167668065+fylsdy@users.noreply.github.com>
2025-11-30 20:58:47 +0800

    Add files via upload

---
+++ 1196590.lua
@@ -0,0 +1,47 @@
+--===================================================================================
+--APPID:1196590;Resident Evil Village
+--===================================================================================
+addappid(1196591,0,"7ccf761ec4f6be4f36416cd1304044deb8b419204f823cae95c9d9f64f94297c")
+addappid(1196592,0,"2ae9966da3659f3b3117057e2b7422a710c9538193866decaf4a7e4a6ac51585")
+addappid(1196593,0,"95a6023be95ec2186ae5340ee6b7e84f093106e5944e1c8ca09b884622dc41e6")
+addappid(1196594,0,"38258feb33272e10fb7214bd6d697ac675b18b968c63a0ee3696a41e2fd50a12")
+addappid(1456360,0,"ba65fa909516f46db9fd95fb6a0e266c4d244b386d260346272290642726b63e")
+addappid(1456361,0,"c847c83484f69cc3195464a7153560fafdd9ec985afb7bf13e2e48ea552dee6d")
+addappid(1456362,0,"46bff99e18ca51b8748333d1dafa1f759b59ca126d648bba42ec8ad386d83827")
+addappid(1456363,0,"39be524780f16cc175cb4e9813d4d4bb50152d4f72be6a4ea0cb23cf7f4669d3")
+addappid(1731080,0,"caa8356d4e747e982b77b28cfcb1df9095bea681dfae768c0a727a3cd96a9279")
+addappid(1196595,0,"6a79254c10ba889017e23108389c63d5689eb6b50b7d7d22524e9a4f1de24460")
+addappid(1731081,0,"69638321e164cb5fd216b2ab217bffa7073fce678124665c60934e972bcf0c2e")
+--===================================================================================
+--APPID:2062120;Resident Evil Village Gold Edition Gameplay Demo
+--===================================================================================
+addappid(2062121,0,"02469bd3fc5909666dcba52047c05778f714ea3f2e9bae40685576223472c104")
+--addappid(2062122,0,"Free on Demand(Only JP)")
+addappid(2062123,0,"ec7a6a8621e893a336faeeef63d3bb5219f5126b8ab0c3bb2ce0c2afea0e9524")
+--addappid(2062124,0,"Free on Demand(Only JP)")
+--===================================================================================
+--APPID:1541780;Resident Evil Village Gameplay Demo
+--===================================================================================
+addappid(1541781,0,"e42ec2717765d55d7663f046064f350ddf83bad2c46d8f834904fa39da7cca30")
+--addappid(1541782,0,"Free on Demand(Only JP)")
+addappid(1541783,0,"8f6fbccef070f9e830b9a55bd76270c1b906726b4187169a8de06b4fda5ce333")
+--addappid(1541784,0,"Free on Demand(Only JP)")
+--===================================================================================
+--APPID:1608280;Resident Evil Village Original Soundtrack
+--===================================================================================
+addappid(1608281,1,"555674ef85c403570c5d053a3f218bcbcd5a1a76ac0d9474a57c1f2a9ce6d654")
+addappid(1608282,1,"d6f2af14f5f29a0b31bfe21ce318c38f9f3069a293235f80f3245034be5cd8b4")
+addappid(1608283,1,"42602529decdad2e6e1baa4cbc078be8ef604912036487806865cf684288265a")
+--addappid(1608284,1,"Need Owner Share From JP Region")
+--addappid(1608285,1,"Need Owner Share From JP Region")
+--addappid(1608286,1,"Need Owner Share From JP Region")
+--===================================================================================
+--OPTIONS
+--eg:
+--addappid(1196590)
+--setManifestid(1608283,"3451179779183755707",201047698)
+--addtoken(2786680,"16612582771884404058")
+--setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+--===================================================================================
+addappid(1196590)
+addappid(1608280)
