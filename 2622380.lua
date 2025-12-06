0884627a455ce259ba105826eaa3ea681ee0f20b
fylsdy <167668065+fylsdy@users.noreply.github.com>
2025-12-06 14:41:08 +0800

    Add files via upload

---
+++ 2622380.lua
@@ -0,0 +1,22 @@
+--====================================================================================
+--APPID:2622380;ELDEN RING NIGHTREIGN
+--====================================================================================
+addappid(2622380,1,"997d257d3a44c80b026acf14bc2c17b5280bec0051510350a580b1f76a7dc403") -- ELDEN RING NIGHTREIGN
+addappid(2622381,1,"c3e9ccfedcda0cd10e53938e15261130b2e166007d2ca8f13aa5a05f06bd2630") -- Depot 2622381
+--addappid(2622382,1,"NO PACKAGES")
+addappid(2622383,1,"c6e496ba5ae5c8bfa358f3a7d923ec39964fa0a41c4fa77006249fe34e677af2") -- Depot 2622383
+addappid(2622384,1,"029f67382779483e0b34bc2f1a1fae303e7f83a4efead9ed076448f386afd28f") -- Depot 2622384
+addappid(3515600,1,"698188f58b73df7953b831a1e83a2b75cce4a3ec24d6c8ea3e3869bed96219dd") -- ELDEN RING NIGHTREIGN Digital Artbook  Mini Soundtrack - Depot 3515600
+addappid(3531720,1,"7869ded55b93c8a360d2b867c0f4dd91e0e1eb8a1de01d69a93c907a736d996c") -- ELDEN RING NIGHTREIGN The Forsaken Hollows - Depot 3531720
+--====================================================================================
+--OPTIONS
+--eg:
+--addappid(1196590)
+--setManifestid(1608283,"3451179779183755707",201047698)
+--setManifestid(1608283,"3451179779183755707")
+--addtoken(2786680,"16612582771884404058")
+--setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+--====================================================================================
+addappid(3319490) -- ELDEN RING NIGHTREIGN Bonus Gesture
+addappid(3515610) -- ELDEN RING NIGHTREIGN - Deluxe Upgrade Pack
+addappid(3637850) -- ELDEN RING NIGHTREIGN CE Content (Retail)
