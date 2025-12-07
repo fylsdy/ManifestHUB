4dcc6616556f91dcfc37eaf7b9704b885157d964
fylsdy <167668065+fylsdy@users.noreply.github.com>
2025-12-07 19:02:37 +0800

    Add files via upload

---
+++ 2799860.lua
@@ -0,0 +1,20 @@
+--====================================================================================
+--APPID:2799860;INAZUMA ELEVEN: Victory Road
+--====================================================================================
+addappid(2799861,1,"da70d57902dfb9488ab7546791fe906d239062bb3b209db909eda0a17e4745c3") -- Depot 2799861
+--====================================================================================
+--APPID:2854160;INAZUMA ELEVEN: Victory Road Worldwide Beta Test Demo "Leave Your Inazuma Mark on the World!"
+--====================================================================================
+--addappid(2854161,1,Free Weekend (Demand)")
+--====================================================================================
+--OPTIONS
+--eg:
+--addappid(1196590)
+--setManifestid(1608283,"3451179779183755707",201047698)
+--setManifestid(1608283,"3451179779183755707")
+--addtoken(2786680,"16612582771884404058")
+--setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+--====================================================================================
+addappid(2799860) -- INAZUMA ELEVEN: Victory Road
+addappid(3550790) -- INAZUMA ELEVEN Victory Road - Edition Upgrade (Deluxe Edition)
+addappid(3801700) -- INAZUMA ELEVEN Victory Road - Pre-order Bonus
