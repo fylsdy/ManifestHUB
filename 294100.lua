71c24d64412644237ca2fde48db36c839405b1c1
fylsdy <167668065+fylsdy@users.noreply.github.com>
2025-12-21 11:34:51 +0800

    Add files via upload

---
+++ 294100.lua
@@ -0,0 +1,72 @@
+--====================================================================================
+-- APPID:294100;RimWorld
+--====================================================================================
+addappid(294100,1,"44498055cce67eea5d84c5b6bc1bd5ca9e8c988963aea857574277fc217cbad0")
+addappid(294101,0,"a5f3f4466343e7297b20a43dde9d6b6ed9d0d4230b74689caf5746d11de0367d")
+addappid(294104,0,"db7d2330e8748efe01ff1674580919b72e40879517e378b568533acc2863ec26")
+addappid(294102,0,"dfa49a9452d912eb031ecd2111c1a9b1331bfe58000b9d68b8d5b646acbd1054")
+addappid(294103,0,"a8218a15388519f5ded274def07d1dd1c535e1bfe646eb17463775bf47f63e39")
+--addappid(367682 	UnusedDLC 367682RimWorld Pirate King (367682) Depot 	No size )	
+--addappid(367680 	UnusedDLC 367680RimWorld Name in Game (367680) Depot 	No size )	
+--addappid(367681 	UnusedDLC 367681RimWorld Backstory in Game (367681) Depot 	No size)
+addappid(1149641,0,"298db7a9d64ecbc820422e53907ab224611829745e8e4b20a4387efba317bee1")
+addappid(1149644,0,"fafd64b303cda2e9b938ce60e7af4653a574b035d59cccd31be1ff5c2dc8c0ee")
+addappid(1149642,0,"14ba0c9d972b57cf45870968dfd396baa35f94d87f117e91e64ba03d25d5d5ad")
+addappid(1149643,0,"a2065b38cf6723c9cc336f68388389a7b437e4404a9856982d2594a928b49b23")
+addappid(294105,0,"be81976289e837bb2cb5c9858b07b9f72612a69414697f44c5b138fb3041a250")
+addappid(294106,0,"d1a9518d7b68f46dc53981fe05514f6daa66a3aa125637c13c1535e8cdb8e4b6")
+addappid(294107,0,"132106f7d7fd90e2cef97d5a01632ca943c2147afcd166c202d51865cf0786e6")
+addappid(294108,0,"80efa15b6a1330572a30a52fc0147fd7fecf36ea2e3acfa8d7db1d78f28c2144")
+addappid(367683,0,"eae351654f6795aae5320150c2801aa853cc2e46412b72bccca2fbf3f50af32a")
+addappid(367684,0,"699e092d84e16c90a62ebfcd68d1c568c4947ae94d538e999fe56e7cc6043942")
+addappid(367685,0,"8a4f6b777fb54e2730c7ace0e4f85ddf4fc58ea3607ad94eb9cf01acb2571446")
+addappid(367686,0,"ddec61094ce592f6db40f3867b99e458ea4ce2af56cb8f62474631b33a96c734")
+addappid(294109,0,"d670f6ba851c27bd62423853e8dd18d5e32b75393b65bf18914103a0b0db63e9")
+addappid(294110,0,"96eea289fb25789e7d7791bcf77e2af4d62eacaa1c05866991b72e75b805069c")
+addappid(294111,0,"62487ea6d7f63684007a6713749fd8fb14c7955e885aa14cda8c5daa5d930f78")
+addappid(294112,0,"69dc90bdbb7c2c47b168606c5b8bde5ab1c6ec88404fa31bfb1faaf2d5c452c3")
+addappid(294113,0,"3a667846a7e65282aae8275c6848ede3e0e48b3959c6045ebc0ceff98db49e0a")
+addappid(294114,0,"76f5a14f8283fd55fe1b8e0eca01a5408e01b23611e70c2cb92a7e303e835646")
+addappid(294115,0,"40C853711DD9329158BF8ACF348FF5B209664FB273949BEE5276501836C32943")
+addappid(294116,0,"17B44D4AC86A4BD0F697320C0B34672092058ABA22133B9B9028E11CFF1C498D")
+--====================================================================================
+-- APPID:3689230;RimWorld - Odyssey Soundtrack
+--====================================================================================
+addappid(3689232,0,"d2065f031bd1063e01ca219492a7d5f8b260e7a1226642e6b01d1b2deef236a4")
+addappid(3689233,0,"543d8b2ff4a6035f1d57db63a8c6720ff5c6c1312e3762f46d7177a1d3cef7b0")
+--====================================================================================
+-- APPID:990430;RimWorld Soundtrack
+--====================================================================================
+addappid(294119,0,"209513ebdd105ce3652c7096aed2661b4b528932420c2bd8ec197605d5862af7")
+addappid(990431,0,"2174f1eb5c39b7d5a0f00b991fdb1bd690f7ea733d1e8cfdc72a42b5361cae8f")
+--====================================================================================
+-- APPID:2914900;RimWorld - Anomaly Soundtrack
+--====================================================================================
+addappid(2914901,0,"03388eeb53afccb2c2f23198aec94273a51c8ee9c74984ad2b22d124cd6d63b6")
+addappid(2914902,0,"339734b6a82d24d9d82f4477ec85e03bd30616bc1e3b3bc72adffe09ed4cb36e")
+--====================================================================================
+-- APPID:1244270;RimWorld - Royalty Soundtrack
+--====================================================================================
+addappid(1244271,0,"cd6458a1d30a3a5ad4b5c51798ed6fc93fa3f0fd52f7c70a4497a79c6b2cc55a")
+addappid(1244272,0,"536335e0e11357bfd7201e3da9da22f404dae296b6e1ab7200973bc74d3a8277")
+--====================================================================================
+-- OPTIONS
+-- eg:
+-- addappid(1196590)
+-- setManifestid(1608283,"3451179779183755707",201047698)
+-- setManifestid(1608283,"3451179779183755707")
+-- addtoken(2786680,"16612582771884404058")
+-- setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+--====================================================================================
+addappid(3689230)
+addappid(2914900)
+addappid(1244270)
+addappid(990430)
+addappid(367680)
+addappid(367681)
+addappid(367682)
+addappid(1149640)
+addappid(1392840)
+addappid(1826140)
+addappid(2380740)
+addappid(3022790)
