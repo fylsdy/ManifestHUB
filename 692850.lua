1762b12271106eca8f4bf60a279c3c48aba4258a
fylsdy <167668065+fylsdy@users.noreply.github.com>
2025-12-28 11:58:35 +0800

    Add files via upload

---
+++ 692850.lua
@@ -0,0 +1,30 @@
+--====================================================================================
+-- APPID:692850;Bloodstained: Ritual of the Night
+--====================================================================================
+--addappid(692851,0,"0B SIZE")
+addappid(692852,0,"54869b8458af02610a6a5a992b8d846c785caebecb4022511caffab33ce69250")
+addappid(1041460,0,"2c19abb45d89483fe9c472010021ec0c562d84d899c95ddec3c790be5b38a5dd")
+addappid(2380800,0,"ea34e52a8ed5dd5224ebc4fc59b61ac0e039e99814be125d48ac84445301784a")
+addappid(2380801,0,"53471e1a66ed778aec25e1d97bb5984842ceaea23e626815dea9a5e444db91fd")
+addappid(2380802,0,"7edcfc402e1ec88ddfa6d6980e93b542bb1e44d25b7c91ede8a5bc2fdba6fe54")
+addappid(2380803,0,"0eed91ff956f249361d9daa24928e52fec0a6f789680cb1a166b78482bb404b6")
+--====================================================================================
+-- APPID:1101300;Bloodstained: Ritual of the Night - Soundtrack
+--====================================================================================
+addappid(1101300,0,"50ee93ff8bfb3d9eb570cc0e08eee68e968c9cd8c19d7038961dcc6738a68fbd")
+addappid(1101301,0,"bd09c580de1639fa13a7995f7008aff6d3e9a44e3db5f743fd2379292f756763")
+--====================================================================================
+-- APPID:2399960;Bloodstained: Ritual of the Night Playtest
+--====================================================================================
+--addappid(2399961;This app is hidden to the public. SteamDB has seen data for this app before, 
+--so for historic purposes it has been kept visible on this page, and may be out of date.)
+--====================================================================================
+-- OPTIONS
+-- eg:
+-- addappid(1196590)
+-- setManifestid(1608283,"3451179779183755707",201047698)
+-- setManifestid(1608283,"3451179779183755707")
+-- addtoken(2786680,"16612582771884404058")
+-- setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+--====================================================================================
+addappid(692850)
