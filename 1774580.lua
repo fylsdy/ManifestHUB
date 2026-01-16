017087039d74ed259f920bd2f889ff1a8d23e8e3
fylsdy <167668065+fylsdy@users.noreply.github.com>
2026-01-17 01:07:12 +0800

    Add files via upload

---
+++ 1774580.lua
@@ -0,0 +1,31 @@
+--====================================================================================
+-- APPID:1774580;STAR WARS Jedi: Survivor™
+--====================================================================================
+addappid(1774581,1,"94d6a0b0e4e36aadffcbc197792207f773276593c72faf9b02b4b4200135a36f")
+addappid(1774582,1,"b6efa1ef672dce88aa6a6a094211c19110ebe78cf04b8bb87f7ba76e7cf15243")
+--addappid(1774583 	German 	195.54 KiB 	44.28 KiB(NEED OWNER SHARE))
+--addappid(1774584 	French 	220.45 KiB 	47.06 KiB(NEED OWNER SHARE))
+--addappid(1774585 	Italian 	209.68 KiB 	45.98 KiB(NEED OWNER SHARE))
+--addappid(1774586 	Korean 	423.30 KiB 	50.48 KiB(NEED OWNER SHARE))
+--addappid(1774587 	Spanish - Spain 	214.57 KiB 	46.11 KiB(NEED OWNER SHARE))
+--addappid(1774588 	Simplified Chinese 	257.92 KiB 	46.52 KiB(NEED OWNER SHARE))
+--addappid(1774589 	Traditional Chinese 	182.25 KiB 	42.23 KiB(NEED OWNER SHARE))
+--addappid(1774592 	Japanese 	305.43 KiB 	50.13 KiB(NEED OWNER SHARE))
+--addappid(1774593 	Polish 	222.43 KiB 	50.03 KiB(NEED OWNER SHARE))
+--addappid(1774594 	Portuguese - Brazil 	187.65 KiB 	43.88 KiB(NEED OWNER SHARE))
+--addappid(1774595 	Spanish - Latin America 	215.20 KiB 	46.34 KiB(NEED OWNER SHARE))
+addappid(3340993,1,"0f76c015b201b1c8249b6a433ef0873f41ef923a5653bef68f10f8cbc051f414")
+--====================================================================================
+-- OPTIONS
+-- eg:
+-- addappid(1196590)
+-- setManifestid(1608283,"3451179779183755707",201047698)
+-- setManifestid(1608283,"3451179779183755707")
+-- addtoken(2786680,"16612582771884404058")
+-- setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+--====================================================================================
+addappid(2161370)
+addappid(2161742)
+addappid(1774580)
+setManifestid(1774582, "6113531931136169727")
+setManifestid(1774581, "6701111524392590510")
