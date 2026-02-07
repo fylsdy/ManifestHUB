40cdb22cdfd996663040f1805f3c1d22be5a1cd4
fylsdy <167668065+fylsdy@users.noreply.github.com>
2026-02-07 15:25:29 +0800

    Add files via upload

---
+++ 3681010.lua
@@ -0,0 +1,32 @@
+--====================================================================================
+-- APPID:3681010;Nioh 3
+--====================================================================================
+addappid(3681011,0,"1c16eb875f0bd40521cc0ddb2ff4eb5772f0b1976252122abc5f9a09ddaaf995") -- Depot 3681011 -- Depot 3681011
+--addappid(3681012,0,"JP STORE ONLY(FILE HASH SAME AS 3681013)")
+addappid(3681013,0,"b97a4048032dc27c3cf1c6887e6069b0f0d51da2c352c671278b00eb4423de35") -- Depot 3681013 -- Depot 3681013
+--====================================================================================
+-- APPID:4198760;Nioh 3 Demo
+--====================================================================================
+addappid(4198761,0,"b28702af62d1e56dcd815ca58dcc495a0f625619106b300f928f27d2abbb7a68")
+addappid(4198762,0,"cb67b98b1ff9358d5fa5c9eb8105ce0b0ef0eb65e87a122cc624ab2f13764fc4")
+addappid(4198763,0,"a9f8c0b709fb69ed8588f0343b10eee878558311eb514c43b99ad5db68b5b1f0")
+--====================================================================================
+-- OPTIONS
+-- eg:
+-- addappid(1196590)
+-- setManifestid(1608283,"3451179779183755707",201047698)
+-- setManifestid(1608283,"3451179779183755707")
+-- addtoken(2786680,"16612582771884404058")
+-- setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+--====================================================================================
+addappid(3681010) -- Nioh 3 -- Nioh 3
+addappid(4003880) -- Nioh 3 - Hellfire Equipment Set (Hellfire Warrior Armor set + Hellfire Ninja Attire set)
+addappid(4003890) -- Nioh 3 - Scampuss Furball Netsuke Charm
+addappid(4003900) -- Nioh 3 - Hellfrost Equipment Set (Hellfrost Warrior Armor set + Hellfrost Ninja Attire set)
+addappid(4003910) -- Nioh 3 - Infernal Weapons Set
+addappid(4003920) -- Nioh 3 - Kodama Netsuke Charm
+addappid(4003930) -- Nioh 3 Season Pass
+addappid(4003940) -- Nioh 3 - Chijiko Netsuke Charm
+addappid(4232000) -- Nioh 3 - Kusanagi Netsuke Charm
+addappid(4232020) -- Nioh 3 - Youngblood Armor
+addappid(4232030) -- Nioh 3 - Lone Wolf's Armor
