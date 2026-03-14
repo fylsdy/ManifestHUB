b8c68ecb550a0ab0eff44270fb804b9903ebcf71
fylsdy <167668065+fylsdy@users.noreply.github.com>
2026-03-14 11:04:01 +0800

    Add files via upload

---
+++ 2852190.lua
@@ -0,0 +1,50 @@
+--====================================================================================
+-- APPID:2852190;Monster Hunter Stories 3: Twisted Reflection
+--====================================================================================
+addappid(2852190,1,"787c31aa0484811d8e5dedc92eb126f7b09ba686c105c57158bcf06beb46e03f")
+addappid(2852191,0,"d9ab76ecfa885be755b04a902f6949a0ee65ed60942bda0caeea1cb8832f65cf")
+addappid(3581280,0,"585336c8945a87126b16997b0323567b357fbbb3cf529cd362fa6ec10f93329d")
+--addappid(3872750 	DLC 3872750SteamDB [Unknown App] 3872750 	6.32 MiB 	6.30 MiB)
+--addappid(3879360 	DLC 3879360SteamDB [Unknown App] 3879360 	1.25 MiB 	1.21 MiB)
+--addappid(3879370 	DLC 3879370SteamDB [Unknown App] 3879370 	1.10 MiB 	1022.33 KiB)
+addappid(3879380,0,"9cc00e11fdaab23c845e9ccfee48a996421bd2fab4647a5fd9acc6fe7335a5c5")
+--addappid(3879390 	DLC 3879390SteamDB [Unknown App] 3879390 	1.27 MiB 	1.20 MiB)
+addappid(3879400,0,"ae5e5e1120dc2085e8f9b68942bc777dbc99b200133bf9b5e673dbafd251d02a")
+--addappid(3879410 	DLC 3879410Accessory: Fang Talisman[NO WAY TO GET] 	6.14 MiB 	6.03 MiB)
+--addappid(3879420 	DLC 3879420SteamDB [Unknown App] 3879420 	2.12 MiB 	1.99 MiB)
+--addappid(3879430 	DLC 3879430Accessory: Protective Band[NO WAY TO GET] 	4.24 MiB 	4.22 MiB)
+--addappid(3879440 	DLC 3879440Accessory: Ornament of Bonds[NO WAY TO GET] 	4.53 MiB 	4.46 MiB)
+addappid(3581290,0,"94d76ae0d0ff4533fc1db50e587a91c84454342672882ed4849f773615336cd6")
+addappid(3581300,0,"0928f3f1790516b082ca620d36abae082684c31f0073341bca492de242831a09")
+addappid(3581310,0,"678544f31185ca208900fc3b509c2b0f0db2f9383ffb3a408cc5b01ff33828e3")
+addappid(3581320,0,"72b08e75337e0436b49187778e9d57c98b43b6944a1b6731d50fcf54018851e0")
+addappid(3581330,0,"5133aff38832e81fbb44ddbcf1be7bed1a60f2bc3512e81cd3cf9d6809be42fc")
+addappid(3581340,0,"28d189a8b1dcf05fe55df9c24f2026603b5b7ce3a851764f9654cd3f9048cb68")
+addappid(3581270,0,"ef13f858536aa4b5c54307a638e5efb7764ab9763ce513f0893d9d752cf458ea")
+addappid(3581380,0,"2de77f7d0978c2524258337ea4eb8837adc735aef087362b12425e26fc06956f")
+addappid(3581390,0,"52ca7186e556b209f821b8df5252d70e4e3f54813b6e68841688e269d9f24255")
+addappid(3581400,0,"0656897589842d8b5f43294dbc6cf1b3322f474f0cd0f44bd15b3b2d8764f8cf")
+addappid(3581410,0,"ddff775d6666c34298b5adb830451180d8c76c47805551cac13d84d1e0de82b1")
+addappid(3581420,0,"7f3529d148dea90208308cc087a305b65934fff1e9c8f904d351a9402a353aff")
+addappid(3581430,0,"15a47d2f5a349aa24e600074429047313e185e288e5399157223d27bb3f97b1b")
+addappid(3581440,0,"f03ec354e423bfaece6ea9f22263abcc5c1afb97fa317f3794989b3f82d6443b")
+addappid(3581450,0,"d45246faa11048ff03056a420f73ff4f80b9ca1dd61a4a77d36b4cd7f53cc51d")
+addappid(3581460,0,"955f11da865a79a14ed09d05caac901334031a7f994cc98bff1bb3b09cb64198")
+addappid(3581470,0,"36d4431ea0b56006c4cef362163e584d212331c31721591efefb66458a208d16")
+addappid(3581480,0,"b42ab35e827d7a2936b4412ad36b56964a8aa00f845590800d722d2c9d6bf9e8")
+--addappid(4283830 	DLC 4283830SteamDB Unknown App 4283830 	[No size] 	)
+--addappid(4007520 	DLC 4007520SteamDB Unknown App 4007520 	[No size] 	)
+--addappid(2852199 		[No size])
+--====================================================================================
+-- APPID:3470630;Monster Hunter Stories 3: Twisted Reflection Trial Version
+--====================================================================================
+addappid(3470631,0,"f8a420416276ba64bec32890b748add7195cb8f946aa0ef022afe11ea05f28bf")
+--====================================================================================
+-- OPTIONS
+-- eg:
+-- addappid(1196590)
+-- setManifestid(1608283,"3451179779183755707",201047698)
+-- setManifestid(1608283,"3451179779183755707")
+-- addtoken(2786680,"16612582771884404058")
+-- setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+--====================================================================================
