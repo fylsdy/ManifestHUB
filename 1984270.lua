62e1d8780f3b5bfc42967bc82a2dd7a242daac10
fylsdy <167668065+fylsdy@users.noreply.github.com>
2026-07-12 00:23:02 +0800

    Add files via upload

---
+++ 1984270.lua
@@ -0,0 +1,35 @@
+--====================================================================================
+-- APPID:1984270;Digimon Story Time Stranger
+--====================================================================================
+addappid(1984270,1,"60f0f52f51592d35101a1678ff221736cee376b3a9db46229f5164dd95b70add")
+addappid(1984271,1,"b0aa8ddfef2996e3122b0bda1d02f7ceb28f608ac8c1d853e6793790aaab8860")
+--addappid(1984272,1,[No size]Unused )
+addappid(3524240,1,"f06dc8a525a2fb5e03453da570b80915bd7c02daae714ac854c86283a2bc4e18")
+addappid(3524250,1,"b6c098c1cdd8687907373e2a6e59446445aafca92e10fe4da2c59481f0448f24")
+addappid(3524260,1,"0d0cb59fa779a35a600989d6bbbd8a43d4459245ff74f28b125e3089d2c59940")
+addappid(3524270,1,"efc19afeba78735ba1acc7264f8363b01d290e19fd5bbde3c9b6956d6dbbd750")
+addappid(3524290,1,"06a3cc7ad7dabf95f282c5ef83ac8eef78605e99999eadcf84aeb73ee22b6eee")
+addappid(3524300,1,"0ae80de6e7b2f3ec166de6a98b3f6a018af9fba6c54f144916d1f4ab07671b2c")
+addappid(3524310,1,"5f8a85a36740263fe78affe346fd7b0c18eb7ad2a51537a2c875f64fa080cbf1")
+addappid(3524320,1,"0da086161abea2f6ce0863a530fefc41ac12993fb4308ff16301926398803a27")
+addappid(3524330,1,"2358d5ff200cefb1eef6f77277f04089a01daaea4418cd48d530b4de3fe53f61")
+addappid(3524340,1,"5441f08ccf7ebad6314bac9d071ba47a41e709f4000e121004752959138bb2dc")
+addappid(3524350,1,"388eb5d2862631b327cd83e312bc69e3f14ecf6087a6a2ec0860f20df764139d")
+addappid(3524360,1,"9ac71aa654d8726aaf514236b138ac5c075854dd8f33abbfeb1b4316302c7f7b")
+addappid(3524370,1,"a20f41adefa6edd0b1a0644a6e359437cc9476624a4e80b6b31d90c893fb3577")
+addappid(3524380,1,"4ef7dc743aea30597adbffc2215a364e119c16ab160dc2f20ae358296d12f4bf")
+--addappid(4486910,1,[No size] 	DLC 4486910SteamDB Unknown App 4486910 )
+--====================================================================================
+-- APPID:3815860;Digimon Story Time Stranger Demo
+--====================================================================================
+addappid(3815861,1,"cef846b11496a9f0f10a21fa7b248d09fae15ade777b842f583570db5274922a")
+--====================================================================================
+-- OPTIONS
+-- eg:
+-- addappid(1196590)
+-- setManifestid(1608283,"3451179779183755707",201047698)
+-- setManifestid(1608283,"3451179779183755707")
+-- addtoken(2786680,"16612582771884404058")
+-- setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+-- setAppEticket(998940,"080210edeb80f404180020542a800178a97e4b46b29bbb2bd779f583b48af94e41357a812de4a125879c4990f49a029c0f8a1fee2dfe362db57d36916dcca613f7129ae4cc310fb5a36dd71b046092cb3ae85cae322b138f15fa61a62ecd6c36e5e8ec91f763022b0fea953c208becdd49f85e16c55b2804023d303fc6eb9980aad829e8dd15c3d79d7d0024277a09")
+--====================================================================================
