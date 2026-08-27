b3e2d151263f5082b36209eaa7db40f12a5cf305
fylsdy <167668065+fylsdy@users.noreply.github.com>
2026-08-27 18:51:01 +0800

    Add files via upload

---
+++ 4356430.lua
@@ -0,0 +1,22 @@
+--====================================================================================
+-- APPID:4356430;NBA 2K27
+--====================================================================================
+addappid(4356431,1,"a1ccdda87579dfa269b9015cc7ba18b7eb8339848b5eb408bfea8bdfb31eb8a4") -- Depot 4356431
+addappid(4687050,1,"c96a0954f3836680afb31e3b837546bb92606ccaebcc3e1bf8cd3761b8221c25") -- Depot 4687050
+--addappid(228988,1,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+--====================================================================================
+-- OPTIONS
+-- eg:
+-- addappid(1196590)
+-- setManifestid(1608283,"3451179779183755707",201047698)
+-- setManifestid(1608283,"3451179779183755707")
+-- addtoken(2786680,"16612582771884404058")
+-- setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+--====================================================================================
+addappid(4356430) -- NBA 2K27
+addtoken(4356430, "13716260120817756824")
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4357560) -- NBA 2K27 Pro Pass Season 1
+addappid(4357570) -- NBA 2K27 Hall of Fame Pass Season 1
+addappid(4357580) -- NBA 2K27 MyCAREER Bonus Offer Season 1
+addappid(4357590) -- NBA 2K27 MyTEAM Bonus Offer Season 1
