bad27cd7e52dc6d2e6e00c896f437a4dc537947f
fylsdy <167668065+fylsdy@users.noreply.github.com>
2026-09-05 19:02:30 +0800

    Add files via upload

---
+++ 2054970.lua
@@ -0,0 +1,64 @@
+--====================================================================================
+-- APPID:2054970;Dragon's Dogma 2
+--====================================================================================
+addappid(2054970,1,"c602a4fd56a58f08d5fceac535f62f17aeeb68e532be0dccbc393393f8dd3783")
+addappid(2054971,1,"792bf9022d9b26150098ba84f896617faee1db94dd4bd50cb9d70fcba3700282")
+--addappid(2054972,1,"251b0eee00d4ff779243b1018a8c6c77664e933ba7ee3753803555cef509480f")
+addappid(2054973,1,"26687268229e99b88102170590ad39df0aa4bbc80190480af56ee3c54c11b1cc")
+--addappid(2054974,1,"4ebc5021915c7e4cc3c5ba1a4a08015289788d290c3ed14a772893ee8ebb6a60")
+addappid(2581000,1,"34f71230c21f8dd552d994e4b39d8f177624b293956965b29c56fe5cad1afba1")
+addappid(2593170,1,"6fb4131955e3f47efd1bc3d718bcba8b98a47551581d0ed68e4175cb02c0762d")
+addappid(2593180,1,"94866b024a8c73d4c231e6399db5db65b8dba78f2b5a7324b953ba24e7580297")
+addappid(2593190,1,"717cfa28b5def3ef62181fa383c948ab68dc5ecf102dc498aa77e17c7c823d67")
+addappid(2593200,1,"675ab608cb1b19781010a72514f455535f7951600ee60e6e900a7cd8cccf3958")
+addappid(2593210,1,"a94822751d8c9f35b041fe98b3766bbda91770fa3e689d7731b55aa770055906")
+addappid(2593220,1,"b4bd996b7a1391fbd2640f3f777dfa348338c316e2644a9106b88ecd2b1cdc18")
+addappid(2593230,1,"aa7db1a358553090e520148471ae0f47c415b4f234a3209fb9079ae5b47cc5a9")
+addappid(2593240,1,"0442eb19917ae6c4ba9b8cf177aab59bea31734ed109f700c910eac48a28e8b3")
+addappid(2593250,1,"ccc0a1a1c5ec9194116dc5b699892d4230f059f0b0ac053e54927446b214fcee")
+addappid(2593260,1,"0fe1b3dc096bdf30e4be2f6afd8f7e5073f1c5ec1b2a773435421a30af259437")
+addappid(2593270,1,"ddc76b8dc500c37d77e1ef1a22be78b9470043c9e9d324a4cea0aa5b405e01e8")
+--addappid(2593280 	[No size] 	DLC 2593280SteamDB Unknown App 2593280 	
+--addappid(2593290  [No size] 		DLC 2593290Dragon’s Dogma 2: Dark Arisen Expansion 
+addappid(2694820,1,"2ff10651137a55b9ea55d673f207d1a8f90d82b128b1eabec8cc296ce2a1ac64")
+addappid(2694830,1,"fa2ad1a149ada24a69bb6a644752e962e197779bef21e3d2b2f55b1746870a97")
+addappid(2694840,1,"8b651167e84540b7b94b5c38533869ee2d15c6705401e5b02f69738c8a9827f0")
+addappid(2694850,1,"8ef6554b2f36cd1f0edb13096eecc0f9aa4dbe9c979d5585bc28acf0f08a85c5")
+addappid(2757060,1,"7eda6a67ceb23166e64a211e8b17e4e16cff894abd4064feb8b0be7ea53418d4")
+addappid(2757080,1,"42737857648e91dadef02a18d35d2d4e33c0b869534e25508f47cb9b17ce22b5")
+addappid(2757090,1,"d3c730e0273b41df400e908f2f19991dee84dacf09f756d250ee3bfcecfc86c1")
+--addappid(2757100 	[NO PACKAGE]DLC 2757100SteamDB Unknown App 2757100 	728 B 	784 B)
+--addappid(2757110 	[NO PACKAGE]DLC 2757110SteamDB Unknown App 2757110)
+addappid(2757120,1,"53e2650ec1e211fcc14e5bab77bc4d54d48e31d6c8da6a97a9b8a91a4fa02a96")
+addappid(2757130,1,"57a6b39b28c0ab8afb7ae9a1ea615edaee3a40530a9882be86b7ad0cadbfdec6")
+addappid(2757140,1,"63fd3570ac475c198512ff0e11cb8d6b6ea4b49746e34b4270de6dac67ac7f13")
+--addappid(2757150 	[NO PACKAGE]DLC 2757150SteamDB Unknown App 2757150 	730 B 	784 B
+--addappid(2757160 	[NO PACKAGE]DLC 2757160SteamDB Unknown App 2757160
+addappid(2757170,1,"005ecbf1bfbad5b8d86810a628037653c0639076bbd85de52088656954a1f670")
+--addappid(2757180 	[NO PACKAGE]DLC 2757180SteamDB Unknown App 2757180 	731 B 	784 B
+--addappid(2757190 	[NO PACKAGE]DLC 2757190SteamDB Unknown App 2757190 	726 B 	784 B
+--addappid(2757200 	[NO PACKAGE]DLC 2757200SteamDB Unknown App 2757200 	729 B 	784 B
+--addappid(2757210 	[NO PACKAGE]DLC 2757210SteamDB Unknown App 2757210 	726 B 	784 B
+--addappid(5184170 	[No size]DLC 5184170SteamDB Unknown App 5184170
+--====================================================================================
+-- APPID:3350020;Dragon's Dogma 2 Original Soundtrack
+--====================================================================================
+addappid(3350021,1,"f909021c49122e54311e89af4a9372aebabe87aee748e00dea186a6599ea74d6")
+--====================================================================================
+-- APPID:2674810;Dragon's Dogma 2 Character Creator & Storage
+--====================================================================================
+--addappid(2674811,1,"e1717e0bfea5acb27240ccc45a2643ceaec2e5eeaaf96c903d601a8c46d09535")
+--addappid(2674812,1,"b24d2b2a6731ba5f2373cdb34715eea0409bbec7c9e8b26436b8675a9886e7bf")
+--====================================================================================
+-- OPTIONS
+-- eg:
+-- addappid(1196590)
+-- setManifestid(1608283,"3451179779183755707",201047698)
+-- setManifestid(1608283,"3451179779183755707")
+-- addtoken(2786680,"16612582771884404058")
+-- setAppticket(3362990,"3800000004000000a185a25201001001ae50330048c019142600010a0000000086e00e6806902a680100022912000100529333000000000005f1bd380a1f7d37667231de28077bbb7736a61430e2457b1cccacc3ac471294d8bcb578bde0d9ca0ce8935515c3dc30cfa6351f0087ec254b4a543546edfd41356702a221b457b53b3ebaa60d6f32ad3e7122e7cde4b341c0372bd7221ccb9ac6a63d0dcf8c73346f8c083ca5003a6f2860bb342d1ec5955b6307c596bc0c0c")
+--====================================================================================
+addappid(2593280)
+addappid(2593290)
+addappid(5184170)
+addappid(3350020)
